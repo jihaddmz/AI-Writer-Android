6180370edbc79103d1b3d969496f42418eb3b118
Jihad Mahfouz <jihadmahfouz8@gmail.com>
2023-07-08 11:27:04 +0300

    - Removed all of the unnecessary elements in the settings

--- app/src/main/java/com/appsfourlife/draftogo/home/presentation/ScreenSettings.kt
+++ app/src/main/java/com/appsfourlife/draftogo/home/presentation/ScreenSettings.kt
@@ -8,6 +8,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.rememberBottomSheetScaffoldState
@@ -17,22 +18,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.appsfourlife.draftogo.R
 import com.appsfourlife.draftogo.components.*
 import com.appsfourlife.draftogo.helpers.*
-import com.appsfourlife.draftogo.home.components.TypeModelChooser
 import com.appsfourlife.draftogo.ui.theme.SpacersSize
 import com.appsfourlife.draftogo.util.BottomNavScreens
 import com.appsfourlife.draftogo.util.Screens
-import com.appsfourlife.draftogo.util.SettingsNotifier
 import com.appsfourlife.draftogo.util.SettingsNotifier.navHostController
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -75,6 +74,16 @@ fun ScreenSettings(
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
 
+                MyUrlImage(
+                    modifier = Modifier.clip(CircleShape),
+                    imageUrl = HelperAuth.auth.currentUser?.photoUrl.toString(),
+                    contentDesc = "",
+                    baseSize = 55.dp
+                )
+                MyText(text = HelperAuth.auth.currentUser?.email!!)
+
+                MySpacer(type = "large")
+
                 AppLogo(
                     modifier = Modifier,
                     imageID = R.drawable.logo,
@@ -86,6 +95,23 @@ fun ScreenSettings(
                 MyText(
                     text = stringResource(id = R.string.app_name),
                     fontWeight = FontWeight.Bold
+                )
+
+                MySpacer(type = "small")
+
+                MyText(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = SpacersSize.large),
+                    text = stringResource(
+                        id = R.string.app_version,
+                        LocalContext.current.packageManager.getPackageInfo(
+                            LocalContext.current.packageName,
+                            0
+                        ).versionName
+                    ),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
                 )
 
                 MySpacer(type = "small")
@@ -102,76 +128,76 @@ fun ScreenSettings(
                     navController.navigate(Screens.ScreenFeedback.route)
                 }
 
-                MySpacer(type = "small")
+//                MySpacer(type = "small")
+//
+//                MyOutlinedButton(text = stringResource(id = R.string.redeem_code)) {
+//                    coroutineScope.launch(Dispatchers.IO) {
+//                        bottomSheetScaffoldState.bottomSheetState.expand()
+//                    }
+//                }
+//            }
 
-                MyOutlinedButton(text = stringResource(id = R.string.redeem_code)) {
-                    coroutineScope.launch(Dispatchers.IO) {
-                        bottomSheetScaffoldState.bottomSheetState.expand()
-                    }
+//            MySpacer(type = "large")
+//
+//            TypeModelChooser()
+//
+//            MySpacer(type = "small")
+//
+//            TypeWriterLength()
+
+//            MySpacer(type = "small")
+//
+//            mySwitch(
+//                modifier = Modifier.padding(end = SpacersSize.small),
+//                label = stringResource(id = R.string.enable_save_outputs),
+//                initialValue = HelperSharedPreference.getIsSavedOutputsEnabled()
+//            ) {
+//                HelperSharedPreference.setBool(
+//                    HelperSharedPreference.SP_SETTINGS,
+//                    HelperSharedPreference.SP_SETTINGS_IS_SAVED_OUTPUTS_ENABLED,
+//                    it
+//                )
+//                SettingsNotifier.enableSheetContent.value = it
+//
+//                if (it) {
+//                    HelperAnalytics.sendEvent("enabled_saved_outputs")
+//                } else {
+//                    HelperAnalytics.sendEvent("disabled_saved_outputs")
+//                }
+//            }
+
+                MySpacer(type = "large")
+
+                val showAccessibilityPermissionRequester = remember {
+                    mutableStateOf(false)
                 }
-            }
+                if (showAccessibilityPermissionRequester.value)
+                    HelperUI.ShowAccessibilityPermissionRequester(false)
 
-            MySpacer(type = "large")
+                mySwitch(
+                    modifier = Modifier.padding(end = SpacersSize.small),
+                    label = stringResource(id = R.string.enable_write_anywhere),
+                    initialValue = HelperPermissions.isAccessibilityEnabled(context = context),
+                    onCheckedChange = {
+                        showAccessibilityPermissionRequester.value = it
+                        if (!it) {
+                            val intent =
+                                Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            // request permission via start activity for result
+                            context.startActivity(intent)
+                        }
+                    })
 
-            TypeModelChooser()
+                MySpacer(type = "large")
 
-            MySpacer(type = "small")
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
-            TypeWriterLength()
-
-            MySpacer(type = "small")
-
-            mySwitch(
-                modifier = Modifier.padding(end = SpacersSize.small),
-                label = stringResource(id = R.string.enable_save_outputs),
-                initialValue = HelperSharedPreference.getIsSavedOutputsEnabled()
-            ) {
-                HelperSharedPreference.setBool(
-                    HelperSharedPreference.SP_SETTINGS,
-                    HelperSharedPreference.SP_SETTINGS_IS_SAVED_OUTPUTS_ENABLED,
-                    it
-                )
-                SettingsNotifier.enableSheetContent.value = it
-
-                if (it) {
-                    HelperAnalytics.sendEvent("enabled_saved_outputs")
-                } else {
-                    HelperAnalytics.sendEvent("disabled_saved_outputs")
-                }
-            }
-
-            MySpacer(type = "small")
-
-            val showAccessibilityPermissionRequester = remember {
-                mutableStateOf(false)
-            }
-            if (showAccessibilityPermissionRequester.value)
-                HelperUI.ShowAccessibilityPermissionRequester(false)
-
-            mySwitch(
-                modifier = Modifier.padding(end = SpacersSize.small),
-                label = stringResource(id = R.string.enable_write_anywhere),
-                initialValue = HelperPermissions.isAccessibilityEnabled(context = context),
-                onCheckedChange = {
-                    showAccessibilityPermissionRequester.value = it
-                    if (!it) {
-                        val intent =
-                            Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        // request permission via start activity for result
-                        context.startActivity(intent)
-                    }
-                })
-
-            MySpacer(type = "large")
-
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                // todo uncomment this when we want to re-enable the paid plans
+                    // todo uncomment this when we want to re-enable the paid plans
 //                val isSubscribed = HelperAuth.isSubscribed()
 //                if (isSubscribed) { // if the user is subscribed
 //                    MyAnnotatedText(
@@ -239,20 +265,7 @@ fun ScreenSettings(
 //
 //                MySpacer(type = "small")
 
-                MyText(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = SpacersSize.large),
-                    text = stringResource(
-                        id = R.string.app_version,
-                        LocalContext.current.packageManager.getPackageInfo(
-                            LocalContext.current.packageName,
-                            0
-                        ).versionName
-                    ),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
-                )
+                }
             }
         }
     }
