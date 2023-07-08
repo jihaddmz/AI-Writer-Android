45a09d2765d3d9d60307846c944e4fdb263ea4ae
Jihad Mahfouz <jihadmahfouz8@gmail.com>
2023-07-08 13:19:47 +0300

    - Removed the back arrow in all of the screens

--- app/src/main/java/com/appsfourlife/draftogo/components/AppBarTransparent.kt
+++ app/src/main/java/com/appsfourlife/draftogo/components/AppBarTransparent.kt
@@ -1,17 +1,15 @@
 package com.appsfourlife.draftogo.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.appsfourlife.draftogo.R
 import com.appsfourlife.draftogo.ui.theme.SpacersSize
 
 @Composable
@@ -24,20 +22,22 @@ fun AppBarTransparent(
         modifier = modifier
             .fillMaxWidth()
             .padding(start = SpacersSize.small, top = SpacersSize.small, bottom = SpacersSize.medium),
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
     ) {
-        IconButton(onClick = {
-            onBackBtnClick()
-
-        }) {
-            MyIcon(
-                iconID = R.drawable.icon_arrow_back,
-                contentDesc = "back",
-                tint = Color.Black
-            )
-        }
-        MySpacer(type = "small", widthOrHeight = "width")
+//        IconButton(onClick = {
+//            onBackBtnClick()
+//
+//        }) {
+//            MyIcon(
+//                iconID = R.drawable.icon_arrow_back,
+//                contentDesc = "back",
+//                tint = Color.Black
+//            )
+//        }
+//        MySpacer(type = "small", widthOrHeight = "width")
         Text(
+            modifier = Modifier.padding(top = SpacersSize.small),
             text = title,
             fontSize = 20.sp,
             fontWeight = FontWeight.ExtraBold,
